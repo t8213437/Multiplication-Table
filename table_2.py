21291886afca24dd52805fd12d1a567aaa291190
徐道婷 <tt8213437@gmail.com>
2015-07-31 09:39:43 +0800

    Create table_2.py

---
+++ table_2.py
@@ -0,0 +1,12 @@
+def mult():
+    for x in range(10):
+        if x == 0:
+            continue
+        for y in range(10):
+            if y == 0:
+                continue
+            print(x * y, end= "\t")
+        print()
+
+if __name__ == "__main__":
+    mult()
